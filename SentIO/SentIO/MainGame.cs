2ea55fa4f851cbebff56f568e09eb3f600473ea9
Peter S <peter@anyline.io>
2020-10-02 22:03:55 +0200

    drawing a font.

---
+++ SentIO/SentIO/MainGame.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace SentIO
+{
+    public class MainGame : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        SpriteFont font;
+        
+        private Size viewSize;
+        private Size screenSize;
+        private float scale;
+
+        public MainGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+
+            Debug.WriteLine("------");
+            Debug.WriteLine(Utils.AssemblyDirectory);
+
+            // window & screen setup
+
+            viewSize = new Size(256, 144);
+            scale = 1.0f;
+            screenSize = new Size((int)(viewSize.Width * scale), (int)(viewSize.Height * scale));
+
+            graphics.PreferredBackBufferWidth = screenSize.Width;
+            graphics.PreferredBackBufferHeight = screenSize.Height;
+
+        }
+
+        
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            font = Content.Load<SpriteFont>("console");
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            spriteBatch.Begin();
+
+            spriteBatch.DrawString(font, "Hello World", Vector2.Zero, Color.White);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ SentIO/SentIO/Utils.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace SentIO
+{
+    public struct Size
+    {
+        private Point p;
+
+        public int Width => p.X;
+        public int Height => p.Y;
+
+        public Size(int w, int h)
+        {
+            p = new Point(w, h);
+        }
+    }
+
+    public static class Utils
+    {
+        public static string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+    }
+}
