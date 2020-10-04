068a6f4bd2c6a6d8c0f355ea4c34ab24e395831d
Simon Parzer <simon.parzer@gmail.com>
2020-10-04 20:42:56 +0200

    SentIOServer update

--- SentIOServer/routes/api.js
+++ SentIOServer/routes/api.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json({
-    id: 2
+    ip: req.ip
+  });
+});
+
+router.get('/player', function(req, res, next) {
+  db.getPlayer(req.ip).then((row) => {
+    console.log("getPlayer...");
+    console.log(row);
+    res.json(row);
+  }).catch((err) =>{
+    console.log(err);
+    res.json({error: err});
   });
 });
 
