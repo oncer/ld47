8553603da995444698dcca01a03d19a0f1d92c10
Simon Parzer <simon.parzer@gmail.com>
2021-12-16 16:58:27 +0100

    publish single file

--- SentIO/package.bat
+++ SentIO/package.bat
@@ -1,7 +1,9 @@
 Pushd "%~dp0"
 rd /S /Q SentIO\bin
-dotnet publish -c Release -r win-x64 /p:PublishReadyToRun=false /p:TieredCompilation=false --self-contained
+dotnet publish -c Release -r win-x64 /p:PublishReadyToRun=false /p:TieredCompilation=false /p:PublishTrimmed=false /p:PublishSingleFile=true --self-contained
 cd SentIO\bin\Release\netcoreapp3.1\win-x64
+xcopy interface.lock publish\
+del publish\SentIO.pdb
 rename publish SentIO
 "C:\Program Files\7-Zip\7z.exe" a SentIO.zip SentIO
 copy SentIO.zip ..\..\..\..\..
